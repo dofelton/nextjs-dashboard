e114accf8e8bff0d01d97a50523bd5ec8f2c6871
dofelton <dofelton@gmail.com>
2024-09-28 17:08:49 -0500

    finish chap11

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -10,7 +10,7 @@ export default async function InvoicesTable({
 }: {
   query: string;
   currentPage: number;
-}) {
+}) { 
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
